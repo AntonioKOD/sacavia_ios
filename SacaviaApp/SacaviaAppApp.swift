79ba07e7afb0e223646c5e3228029f9ddab770b3
Antonio Kodheli <antonio_kodheli@icloud.com>
2025-07-16 10:59:25 -0400

    Initial Commit

---
+++ SacaviaApp/SacaviaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SacaviaAppApp.swift
+//  SacaviaApp
+//
+//  Created by Antonio Kodheli on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SacaviaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SacaviaAppTests/SacaviaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SacaviaAppTests.swift
+//  SacaviaAppTests
+//
+//  Created by Antonio Kodheli on 7/16/25.
+//
+
+import Testing
+@testable import SacaviaApp
+
+struct SacaviaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
